f1cd093d88d39b58a3e8fbcfc61b9384dc347ebd
Pakin Panawattanakul <pakin.pan@proton.me>
2026-06-18 11:09:58 +0700

    test permutation

--- kyber768/test_hash.h
+++ kyber768/test_hash.h
@@ -1,12 +1,12 @@
+#ifndef TEST_HASH_H
+#define TEST_HASH_H
+
 #include <stdio.h>
 #include <inttypes.h>
-#include "polyvec.h"
 #include "params.h"
-#include "cbd.h"
-#include "indcpa.h"
 #include "fips202.h"
 #include "symmetric.h"
 
-void test_matrix_gen();
-void test_hash(poly *test);
-void test_keccak_absorb();
+void test_permutation();
+
+#endif
